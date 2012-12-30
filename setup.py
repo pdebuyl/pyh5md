a3aab39a80806c21b5d27ea2573d5215e5bf41fe
Pierre de Buyl <pdebuyl@nononospam.seriously.ulb.ac.be>
2012-12-30 20:35:46 +0100

    Corrected erroneous 'summary' for 'description'.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name="pyh5md", version="0.1", 
-      summary="pyh5md - Read and write H5MD files", 
+      description="pyh5md - Read and write H5MD files",
       author="Pierre de Buyl", author_email="pdebuyl at ulb dot ac dot be", 
       maintainer="Pierre de Buyl", 
       maintainer_email="pdebuyl at ulb dot ac dot be", 
