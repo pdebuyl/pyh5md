b12e50dd8efdea7906264bade52a72949011b9e1
Pierre de Buyl <pdebuyl@nononospam.seriously.ulb.ac.be>
2013-04-27 14:02:21 +0200

    Addition of check_file.py that performs basic checks on a H5MD file.

---
+++ examples/check_file.py
@@ -0,0 +1,19 @@
+# Copyright 2012-2013 Pierre de Buyl
+#
+# This file is part of pyh5md
+#
+# pyh5md is free software and is licensed under the modified BSD license (see
+# LICENSE file).
+
+import numpy as np
+import pyh5md
+from pyh5md.core import is_h5md
+import h5py
+
+f = pyh5md.H5MD_File('poc.h5', 'r')
+
+f.check()
+
+pyh5md.walk(f.f['trajectory'])
+
+f.f.close()
