0fcba292045864c506c41dd2c328ca7d3de955bd
Pierre de Buyl <pdebuyl@nononospam.seriously.ulb.ac.be>
2013-04-27 14:03:39 +0200

    Update of run_h5md.py example and addition of companion analyze_h5md.py.

---
+++ examples/analyze_h5md.py
@@ -0,0 +1,9 @@
+
+import numpy as np
+import pyh5md
+import matplotlib.pyplot as plt
+
+f = pyh5md.H5MD_File('poc.h5', 'r')
+com = f.observable('center_of_mass')
+plt.plot(com.t, com.v)
+plt.show()
--- examples/run_h5md.py
+++ examples/run_h5md.py
@@ -10,12 +10,30 @@ import pyh5md
 
 f = pyh5md.H5MD_File('poc.h5', 'w', creator='run_h5md', creator_version='0', author='Pierre de Buyl')
 
+# Creating atom group
 at = f.trajectory_group('atoms')
 
+# Creating position data
 r = np.zeros((100,3), dtype=np.float64)
-
 at_pos = at.trajectory('position', r.shape, r.dtype)
 
-at_pos.append(r, 0, 0.)
+# Creating velocity data
+v = np.zeros((100,3), dtype=np.float64)
+at_v = at.trajectory('velocity', v.shape, v.dtype)
+
+# Create an observable
+obs_com = f.observable('center_of_mass', r.shape[-1:], r.dtype)
+
+DT = 0.1
+time = 0.
+for t in xrange(201):
+    if t%10==0:
+        at_pos.append(r, t, time)
+        at_v.append(v, t, time)
+        obs_com.append(r.mean(axis=0), t, time)
+    r += DT*0.5*v
+    v += DT*np.random.normal(0., 1., v.shape)
+    r += DT*0.5*v
+    time += DT
 
 f.f.close()
