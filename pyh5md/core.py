89f9eb3c006765641b69dd8f150333b05d8adde8
Konrad Hinsen <konrad.hinsen@fastmail.net>
2013-11-05 17:10:26 +0100

    Allow the creation of time-independent observables

--- pyh5md/core.py
+++ pyh5md/core.py
@@ -225,11 +225,14 @@ class H5MD_File(object):
         it will be created."""
         return ParticlesGroup(self.f, group_name)
 
-    def observable(self, obs_name,*args,**kwargs):
+    def observable(self, obs_name, time=True, *args,**kwargs):
         """Returns observable data as a TimeData object."""
         if 'observables' not in self.f.keys():
             self.f.create_group('observables')
-        return TimeData(self.f['observables'],obs_name,*args,**kwargs)
+        if time:
+            return TimeData(self.f['observables'],obs_name,*args,**kwargs)
+        else:
+            return FixedData(self.f['observables'],obs_name,*args,**kwargs)
         
     def check(self):
         """Checks the file conformance."""
