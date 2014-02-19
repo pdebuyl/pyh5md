7740307fe29113ef781c9a3ae5f6f4e28b864c61
Pierre de Buyl <pdebuyl@ulb.ac.be>
2014-02-19 11:30:14 +0100

    add long_desc

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from distutils.core import setup
 setup(name="pyh5md",
       version="0.1",
       description="pyh5md - Read and write H5MD files",
+      long_desc = \
+"""
+pyh5md is built on top of h5py, the Python HDF5 library by Andrew Colette, to
+read and write easily H5MD files. H5MD is a file format specification based
+on HDF5 to store molecular data, see http://nongnu.org/h5md/
+""",
       author="Pierre de Buyl",
       author_email="pdebuyl at ulb dot ac dot be",
       maintainer="Pierre de Buyl",
