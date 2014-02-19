471a0bc4e061e00743e10893757b1ce4c512a714
Pierre de Buyl <pdebuyl@ulb.ac.be>
2014-02-19 10:44:59 +0100

    one line per setup item.

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from distutils.core import setup
 
-setup(name="pyh5md", version="0.1", 
+setup(name="pyh5md",
+      version="0.1",
       description="pyh5md - Read and write H5MD files",
-      author="Pierre de Buyl", author_email="pdebuyl at ulb dot ac dot be", 
-      maintainer="Pierre de Buyl", 
-      maintainer_email="pdebuyl at ulb dot ac dot be", 
-      license="BSD", url="http://github.com/pdebuyl/pyh5md", 
-      packages=["pyh5md"])
+      author="Pierre de Buyl",
+      author_email="pdebuyl at ulb dot ac dot be",
+      maintainer="Pierre de Buyl",
+      maintainer_email="pdebuyl at ulb dot ac dot be",
+      license="BSD",
+      url="http://github.com/pdebuyl/pyh5md",
+      packages=["pyh5md"],
+      install_requires=['h5py'],
+)
 
