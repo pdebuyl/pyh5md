be5f463f7e7ccb6d30dd69f12a1f821d5864ed4c
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-06-15 09:12:49 +0200

    update for File constructor, time storage and connectivity

--- run.py
+++ run.py
@@ -4,10 +4,10 @@ from h5md_module import File, element
 N = 32
 DT = 0.1
 
-with File('hop.h5', 'w') as f:
-    f.fill_h5md('Pierre', 'run.py', 'N/A')
+with File('example_for_1.1.h5', 'w',author='Pierre', creator='run.py') as f:
 
     f.observables = f.require_group('observables')
+    f.connectivity = f.require_group('connectivity')
     v_e = element(f.observables, 'v', store='linear', data=(1,), step=10, step_offset=10, time=5., time_offset=5.)
 
     f.all = f.particles_group('all')
@@ -27,12 +27,22 @@ with File('hop.h5', 'w') as f:
     mass = np.ones((N,))*100.0
     element(f.all, 'mass', store='fixed', data=mass)
 
+    idx_all = np.arange(N)
+    idx_list = np.random.choice(idx_all, 5)
+    idx_e = element(f.observables, 'idx_list', store='linear', step=10, data=idx_list)
+    idx_e.attrs['particles_group'] = f.all.ref
+
+    pairs = np.random.randint(0, N, (11, 2))
+    pairs_e = element(f.connectivity, 'random_pairs', store='fixed', data=pairs)
+    pairs_e.attrs['particles_group'] = f.all.ref
+
     record = frozenset([0, 1, 2, 10])
     for step in range(21):
         force = np.random.random(pos.shape)-0.5
         vel += force*DT*0.5/mass.reshape((-1,1))
         pos += vel*DT
         vel += force*DT*0.5/mass.reshape((-1,1))
+        idx_list = np.random.choice(idx_all, 5)
         if step in record:
             f.all.box.edges.append((1,1,1), step)
             pos_e.append(pos, step)
@@ -40,4 +50,6 @@ with File('hop.h5', 'w') as f:
             force_e.append(vel, step, step*DT)
         if step%v_e['step'][()] == 0:
             v_e.append(np.random.randint(10))
+        if step%idx_e['step'][()] == 0:
+            idx_e.append(idx_list, step)
 
