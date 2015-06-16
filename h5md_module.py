541b4818a6b3a4daa9b4d592883cd7ea20c80a9e
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-06-16 10:06:05 +0200

    add region argument to apped routines

--- h5md_module.py
+++ h5md_module.py
@@ -70,9 +70,12 @@ class LinearElement(h5py.Group, Element):
     @property
     def element_type(self):
         return 'LinearElement'
-    def append(self, v, step=None, time=None):
+    def append(self, v, step=None, time=None, region=None):
         self.value.resize(self.value.shape[0]+1, axis=0)
-        self.value[-1] = v
+        if region is not None:
+            self.value[-1,region[0]:region[1],...] = v
+        else:
+            self.value[-1] = v
     def get_by_idx(self, idx):
         return self['value'][idx]
 
@@ -118,9 +121,12 @@ class TimeElement(h5py.Group, Element):
                 self.time_offset = None
             self.value = g['value']
         super(TimeElement, self).__init__(g._id)
-    def append(self, v, step, time=None):
+    def append(self, v, step, time=None, region=None):
         self.value.resize(self.value.shape[0]+1, axis=0)
-        self.value[-1] = v
+        if region is not None:
+            self.value[-1,region[0]:region[1],...] = v
+        else:
+            self.value[-1] = v
         if self.own_step:
             self.step.resize(self.step.shape[0]+1, axis=0)
             self.step[-1] = step
