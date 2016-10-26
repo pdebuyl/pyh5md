bbbdcf24a4eaf763590a037d6e98d296c82a8858
Pierre de Buyl <pdebuyl@pdebuyl.be>
2016-10-26 11:02:13 +0200

    fix continuation line

--- pyh5md/__init__.py
+++ pyh5md/__init__.py
@@ -1,2 +1,2 @@
-from .h5md_module import File, element, ParticlesGroup, FixedElement,
-TimeElement, LinearElement
+from .h5md_module import (File, element, ParticlesGroup, FixedElement,
+                          TimeElement, LinearElement)
