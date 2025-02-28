a0e01af50d2b11858eb6b067e0d862de2250dfc0
Pierre de Buyl <pdebuyl@pdebuyl.be>
2025-02-28 10:50:35 +0100

    Change to pyproject.toml

--- pyh5md/__init__.py
+++ pyh5md/__init__.py
@@ -1,2 +1,6 @@
 from .h5md_module import (File, element, ParticlesGroup, FixedElement,
                           TimeElement, LinearElement)
+import os.path
+
+with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as f:
+    __version__ = f.read().strip()
